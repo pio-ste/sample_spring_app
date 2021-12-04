91a50b14aee4d3d0b55babd1628febd59bc0ccf2
pio-ste <pioste97@gmail.com>
2021-12-04 19:46:43 +0100

    First commit

---
+++ src/main/java/com/example/samplespringapp/SampleSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringAppApplication.class, args);
+    }
+
+}
